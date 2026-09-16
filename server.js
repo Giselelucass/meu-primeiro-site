0bd7bcc8e17ecd17ec7196d540c70fd3f08dd0e9
Gisele <giselepinheirigarcia@hotmail.com>
2026-09-16 20:08:29 -0300

    Corrige servidor

--- server.js
+++ server.js
@@ -33,5 +33,5 @@ app.post("/login", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(Servidor rodando na porta ${PORT});
+console.log("Servidor rodando na porta " + PORT);
 });
